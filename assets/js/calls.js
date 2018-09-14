164432da3b54c16b5833ef431d8253daf53d949f
Nicholas A Spangler <palladiate@palladiate.com>
2018-09-14 13:11:47 -0400

    Final changes to add the main page
    Landing page and Swag for Peek service

---
+++ assets/js/calls.js
@@ -0,0 +1,4 @@
+/*
+ * Copyright (c) 2018. Spectrum Communications. ALL RIGHTS RESERVED
+ */
+
--- main.go
+++ main.go
@@ -3,8 +3,24 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+	_ "github.webapps.rr.com/nspangler/agentSkill/docs"
 	"github.webapps.rr.com/nspangler/agentSkill/models"
 )
+
+// Peek Agent Info Service
+
+// @version 2.0
+// @description Service for return Agent login information for Avaya
+// @termsOfService
+
+// @contact.name Nicholas Spangler
+// @contact.url
+// @contact.email nicholas.spangler@charter.com
+
+// @host localhost:9005
+// @BasePath /peek
 
 var ds models.Datastore
 
@@ -15,22 +31,36 @@ func main() {
 	defer ds.Close()
 
 	r := InitRouter()
+	r.LoadHTMLGlob("templates/*")
 
-	/*
-	skillService := r.Group("/peek/v2/switch")
+	loginService := r.Group("/peek")
 	{
-		skillService.GET("/",ReturnSwitches)
-		skillService.GET("/:switch",ReturnAgents)
-		skillService.GET("/:switch/:agent",ReturnAgentInformation)
-	}
-	*/
+		loginService.GET("/",Index)
+		loginService.Static("/assets", "./assets")
 
-	loginService := r.Group("/peek/v2/login")
-	{
-		loginService.GET("/:login", SearchLogin)
+		v2 := loginService.Group("/v2")
+		{
+
+			v2.GET("/switches", ReturnSwitches)
+			v2.GET("/switches/agents", ReturnAgents)
+			v2.GET("/login/:login", SearchLogin)
+
+		}
+
+		// set Swagger route
+		loginService.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	}
+
 
 	r.Run(":9005")
+}
+
+func Index(c *gin.Context) {
+	c.HTML(200, "index",
+		gin.H{
+			"title": "Peek Agent Information Service",
+		},
+	)
 }
 
 func InitRouter() (r *gin.Engine) {
@@ -51,16 +81,15 @@ func ReturnAgents(c *gin.Context) {
 // Search Login Service
 
 // @Summary Finds a PID and returns the full account detail object
-// @Description This service provides full Avaya switch and skill information when provided a
-// Charter PID
+// @Description This service provides full Avaya switch and skill information when provided a PID.
 // @Accept json
 // @Product json
-// @Param Login path string true "PID"
+// @Param login path string true "PID"
 // @Success 200 {object} models.Agent
-// @Failure 404 {object} error
-// @Failure 400 {object} error
-// @Failure 500 {object} error
-// @Router / [GET]
+// @Failure 404 {string} string
+// @Failure 400 {string} string
+// @Failure 500 {string} string
+// @Router /v2/login/{login} [get]
 func SearchLogin(c *gin.Context) {
 	agent, err := ds.GetAgent(c.Param("login"))
 	if err != nil {
